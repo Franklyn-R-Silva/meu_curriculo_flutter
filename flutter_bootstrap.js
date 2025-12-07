c2c4260d0d4e0953b2482453717d1e14f77b48f4
Franklyn-R-Silva <Franklyn-R-Silva@users.noreply.github.com>
2025-12-07 14:08:24 +0000

    deploy: e5be4dceae80c3b9f8f317b544da5102f1306ea4

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a5cb96369ef86c7e85abf5d662a1ca5d897750
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1590616611"
+    serviceWorkerVersion: "4148488113"
   }
 });
